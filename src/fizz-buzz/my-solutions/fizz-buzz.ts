b9c301cfe6968c3a1247c6d878fcf73dc7ce8a36
acalvom <andreac.3108@gmail.com>
2024-04-16 13:11:42 +0200

    refactor: reduce code

--- src/fizz-buzz/my-solutions/fizz-buzz.ts
+++ src/fizz-buzz/my-solutions/fizz-buzz.ts
@@ -1,10 +1,14 @@
 export const getFizzBuzz = (input: number) => {
   const arrayOfZeros = new Array(input).fill(0)
   const arrayOfNumbers = arrayOfZeros.map((item, index) => {
-    if ((index + 1) % 3 === 0 && (index + 1) % 5 === 0) return 'fizzbuzz'
-    else if ((index + 1) % 3 === 0) return 'fizz'
-    else if ((index + 1) % 5 === 0) return 'buzz'
-    else return item + index + 1
+    const position = index + 1
+    return position % 3 === 0 && position % 5 === 0
+      ? 'fizzbuzz'
+      : position % 3 === 0
+      ? 'fizz'
+      : position % 5 === 0
+      ? 'buzz'
+      : item + position
   })
   return arrayOfNumbers
 }
