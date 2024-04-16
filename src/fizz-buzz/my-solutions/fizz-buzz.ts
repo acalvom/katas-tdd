30c7edcbd53e55deb4a8b3d78ec8057c210248c5
acalvom <andreac.3108@gmail.com>
2024-04-16 13:04:58 +0200

    feat: implement feature to get fizzbuzz & fix error (tests pass)

--- src/fizz-buzz/my-solutions/fizz-buzz.ts
+++ src/fizz-buzz/my-solutions/fizz-buzz.ts
@@ -1,8 +1,9 @@
 export const getFizzBuzz = (input: number) => {
   const arrayOfZeros = new Array(input).fill(0)
   const arrayOfNumbers = arrayOfZeros.map((item, index) => {
-    if ((index + 1) / 3 === 1) return 'fizz'
-    else if ((index + 1) / 5 === 1) return 'buzz'
+    if ((index + 1) % 3 === 0 && (index + 1) % 5 === 0) return 'fizzbuzz'
+    else if ((index + 1) % 3 === 0) return 'fizz'
+    else if ((index + 1) % 5 === 0) return 'buzz'
     else return item + index + 1
   })
   return arrayOfNumbers
