e7ca7156fdd676e8583bd9b2ebbf3b455597402b
acalvom <andreac.3108@gmail.com>
2024-04-16 12:59:41 +0200

    feat: implement feature to get buzz (test passes)

--- src/fizz-buzz/my-solutions/fizz-buzz.ts
+++ src/fizz-buzz/my-solutions/fizz-buzz.ts
@@ -1,7 +1,9 @@
 export const getFizzBuzz = (input: number) => {
   const arrayOfZeros = new Array(input).fill(0)
   const arrayOfNumbers = arrayOfZeros.map((item, index) => {
-    return (index + 1) / 3 === 1 ? 'fizz' : item + index + 1
+    if ((index + 1) / 3 === 1) return 'fizz'
+    else if ((index + 1) / 5 === 1) return 'buzz'
+    else return item + index + 1
   })
   return arrayOfNumbers
 }
