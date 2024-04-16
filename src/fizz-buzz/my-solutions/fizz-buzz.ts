ae0fc6f9aa0bd25e39498721a9d2d1af8906cbb9
acalvom <andreac.3108@gmail.com>
2024-04-16 12:56:26 +0200

    feat: implement feature to get fizz

--- src/fizz-buzz/my-solutions/fizz-buzz.ts
+++ src/fizz-buzz/my-solutions/fizz-buzz.ts
@@ -1,5 +1,7 @@
 export const getFizzBuzz = (input: number) => {
   const arrayOfZeros = new Array(input).fill(0)
-  const arrayOfNumbers = arrayOfZeros.map((item, index) => item + index + 1)
+  const arrayOfNumbers = arrayOfZeros.map((item, index) => {
+    return (index + 1) / 3 === 1 ? 'fizz' : item + index + 1
+  })
   return arrayOfNumbers
 }
