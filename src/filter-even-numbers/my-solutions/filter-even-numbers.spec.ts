5f4eeb09986d02e777824b9ffd6d944b1789811d
acalvom <andreac.3108@gmail.com>
2024-04-16 13:22:01 +0200

    test: no filter in odd numbers array (passes)

---
+++ src/filter-even-numbers/my-solutions/filter-even-numbers.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+import { filterEvenNumbers } from './filter-even-numbers'
+
+describe('getFizzBuzz', () => {
+  it('should not filter any number gicen an array of odd numbers ', () => {
+    const given = [1, 3, 5, 7]
+    const actual = filterEvenNumbers(given)
+
+    expect(actual).toEqual([1, 3, 5, 7])
+  })
+})
