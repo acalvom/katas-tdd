fef4a0a81e02e7f7f2514d176b51f94d02587c1b
acalvom <andreac.3108@gmail.com>
2024-04-16 13:23:54 +0200

    test: add test to filter even numbers (fails)

--- src/filter-even-numbers/my-solutions/filter-even-numbers.spec.ts
+++ src/filter-even-numbers/my-solutions/filter-even-numbers.spec.ts
@@ -2,10 +2,17 @@ import { describe, expect, it } from 'vitest'
 import { filterEvenNumbers } from './filter-even-numbers'
 
 describe('getFizzBuzz', () => {
-  it('should not filter any number gicen an array of odd numbers ', () => {
+  it('should not filter any number given an array of odd numbers ', () => {
     const given = [1, 3, 5, 7]
     const actual = filterEvenNumbers(given)
 
     expect(actual).toEqual([1, 3, 5, 7])
+  })
+
+  it('should filter all number given an array of even numbers ', () => {
+    const given = [2, 4, 6, 8]
+    const actual = filterEvenNumbers(given)
+
+    expect(actual).toEqual([])
   })
 })
