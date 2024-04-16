0026ff698e93a1cc7ab8cfabec02d5f21b6e88fb
acalvom <andreac.3108@gmail.com>
2024-04-16 12:56:01 +0200

    test: add test to get fizz (fails)

--- src/fizz-buzz/my-solutions/fizz-buzz.spec.ts
+++ src/fizz-buzz/my-solutions/fizz-buzz.spec.ts
@@ -8,4 +8,11 @@ describe('getFizzBuzz', () => {
 
     expect(actual).toEqual([1, 2])
   })
+
+  it('should get the array of numbers from 1 to 2 and fizz in the 3rd position', () => {
+    const given = 3
+    const actual = getFizzBuzz(given)
+
+    expect(actual).toEqual([1, 2, 'fizz'])
+  })
 })
