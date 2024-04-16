15f6e3e228e20b40260f7ff6ed64c474c64a2fe6
acalvom <andreac.3108@gmail.com>
2024-04-16 13:04:04 +0200

    test: add test to get fizzbuzz

--- src/fizz-buzz/my-solutions/fizz-buzz.spec.ts
+++ src/fizz-buzz/my-solutions/fizz-buzz.spec.ts
@@ -22,4 +22,27 @@ describe('getFizzBuzz', () => {
 
     expect(actual).toEqual([1, 2, 'fizz', 4, 'buzz'])
   })
+
+  it('should get the array of numbers from 1 to 2, fizz in the 3rd position, buzz in the 5th and fizzbuzz in 15th', () => {
+    const given = 15
+    const actual = getFizzBuzz(given)
+
+    expect(actual).toEqual([
+      1,
+      2,
+      'fizz',
+      4,
+      'buzz',
+      'fizz',
+      7,
+      8,
+      'fizz',
+      'buzz',
+      11,
+      'fizz',
+      13,
+      14,
+      'fizzbuzz'
+    ])
+  })
 })
