a6472d2d89dca818de749582d8403fcfed7fe1b0
acalvom <andreac.3108@gmail.com>
2024-04-19 11:40:00 +0200

    feat: handle start > end

--- src/range/my-solutions/range.ts
+++ src/range/my-solutions/range.ts
@@ -1,7 +1,15 @@
 export const range = (end: number, start: number = 0) => {
-  const range = Array.from({ length: end - start + 1 }, (_, index: number) => {
-    return index + start
-  })
+  let range: number[] = []
+
+  if (start <= end) {
+    range = Array.from({ length: end - start + 1 }, (_, index: number) => {
+      return start + index
+    })
+  } else {
+    range = Array.from({ length: start - end + 1 }, (_, index: number) => {
+      return start - index
+    })
+  }
 
   return range
 }
