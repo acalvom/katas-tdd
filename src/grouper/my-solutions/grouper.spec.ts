44edd5ea89e824cde22517a69f9c88adc8440d94
acalvom <andreac.3108@gmail.com>
2024-04-19 10:28:35 +0200

    test: different people with different ages (pass)

--- src/grouper/my-solutions/grouper.spec.ts
+++ src/grouper/my-solutions/grouper.spec.ts
@@ -18,4 +18,20 @@ describe('grouper', () => {
 
     expect(actual).toEqual({ 42: ['Michael Scott', 'Phyllis Smith'] })
   })
+
+  it('should return an object with array of names given array of people with different ages', () => {
+    const given: Person[] = [
+      { name: 'Michael Scott', age: 42 },
+      { name: 'Jim Halpert', age: 34 },
+      { name: 'Pam Beesly', age: 33 },
+      { name: 'Phyllis Smith', age: 42 }
+    ]
+    const actual = grouper(given)
+
+    expect(actual).toEqual({
+      42: ['Michael Scott', 'Phyllis Smith'],
+      34: ['Jim Halpert'],
+      33: ['Pam Beesly']
+    })
+  })
 })
