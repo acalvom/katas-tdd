72f16f71cb50bfb3c9f343503a766d92a50c3c5b
acalvom <andreac.3108@gmail.com>
2024-04-19 10:20:31 +0200

    test: two people with same age (fails)

--- src/grouper/my-solutions/grouper.spec.ts
+++ src/grouper/my-solutions/grouper.spec.ts
@@ -8,4 +8,14 @@ describe('grouper', () => {
 
     expect(actual).toEqual({ 42: ['Michael Scott'] })
   })
+
+  it('should return an object with array of names given array of 2 people with same age', () => {
+    const given: Person[] = [
+      { name: 'Michael Scott', age: 42 },
+      { name: 'Phyllis Smith', age: 42 }
+    ]
+    const actual = grouper(given)
+
+    expect(actual).toEqual({ 42: ['Michael Scott', 'Phyllis Smith'] })
+  })
 })
