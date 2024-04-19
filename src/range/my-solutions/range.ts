ff76b5bc6902cd97978eae080b0db31a67aad42f
acalvom <andreac.3108@gmail.com>
2024-04-19 11:42:43 +0200

    refactor: minimize code

--- src/range/my-solutions/range.ts
+++ src/range/my-solutions/range.ts
@@ -1,15 +1,4 @@
-export const range = (end: number, start: number = 0) => {
-  let range: number[] = []
-
-  if (start <= end) {
-    range = Array.from({ length: end - start + 1 }, (_, index: number) => {
-      return start + index
-    })
-  } else {
-    range = Array.from({ length: start - end + 1 }, (_, index: number) => {
-      return start - index
-    })
-  }
-
-  return range
-}
+export const range = (end: number, start: number = 0) =>
+  start <= end
+    ? Array.from({ length: end - start + 1 }, (_, index: number) => start + index)
+    : Array.from({ length: start - end + 1 }, (_, index: number) => start - index)
