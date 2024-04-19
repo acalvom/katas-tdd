f1a1c394401747c5ad8e8fecc860c5e1d06b9464
acalvom <andreac.3108@gmail.com>
2024-04-19 11:15:11 +0200

    feat: implement feature when start != 1 (pass)

--- src/range/my-solutions/range.ts
+++ src/range/my-solutions/range.ts
@@ -1,5 +1,5 @@
 export const range = (start: number, end: number) => {
-  const range = Array.from({ length: end }, (_, index: number) => {
+  const range = Array.from({ length: end - start + 1 }, (_, index: number) => {
     return index + start
   })
 
