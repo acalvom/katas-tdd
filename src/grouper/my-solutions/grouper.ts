d5b90f3e0a1e13ad5f469b4dc70ef9ca0012c6e2
acalvom <andreac.3108@gmail.com>
2024-04-19 10:30:10 +0200

    refactor: minimize code

--- src/grouper/my-solutions/grouper.ts
+++ src/grouper/my-solutions/grouper.ts
@@ -1,18 +1,18 @@
 type Name = string
 type Age = number
 
+export type Group = Record<Age, Name[]>
+
 export interface Person {
   name: Name
   age: Age
 }
 
-export type Group = Record<Age, Name[]>
-
-export const grouper = (people: Person[]) => {
-  const group = people.reduce((group: Group, person: Person) => {
-    if (!group[person.age]) return { ...group, [person.age]: [person.name] }
-    else return { ...group, [person.age]: [...group[person.age], person.name] }
-  }, {})
-
-  return group
-}
+export const grouper = (people: Person[]) =>
+  people.reduce(
+    (group: Group, person: Person) =>
+      !group[person.age]
+        ? { ...group, [person.age]: [person.name] }
+        : { ...group, [person.age]: [...group[person.age], person.name] },
+    {}
+  )
