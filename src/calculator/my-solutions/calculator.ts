7cb5735da0fce27a237d980b54a064f1a67cf523
acalvom <andreac.3108@gmail.com>
2024-05-10 10:26:49 +0200

    fix: string handler

--- src/calculator/my-solutions/calculator.ts
+++ src/calculator/my-solutions/calculator.ts
@@ -1,14 +1,17 @@
 export const calculator = (str: string): number => {
-  const charArray: string = str.charAt(0).includes('+ -')
-    ? str.replaceAll(' ', '')
-    : '+' + str.replaceAll(' ', '')
+  const charArray: string =
+    str.startsWith('+') || str.startsWith('-')
+      ? str.replaceAll(' ', '')
+      : '+' + str.replaceAll(' ', '')
   const numberArray: number[] = []
   let operator: string = ''
 
   for (const char of charArray) {
-    if (!(char === '+' || char === '-'))
+    if (!(char === '+' || char === '-')) {
       operator === '+' ? numberArray.push(Number(char)) : numberArray.push(-Number(char))
-    else operator = char
+    } else {
+      operator = char
+    }
   }
 
   return numberArray.reduce((acc, curr) => curr + acc, 0)
