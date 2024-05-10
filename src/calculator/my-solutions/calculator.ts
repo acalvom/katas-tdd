67902673d588c0007a19d36728775bd8c80bcac9
acalvom <andreac.3108@gmail.com>
2024-05-10 08:32:37 +0200

    feat: implement add two numbers

--- src/calculator/my-solutions/calculator.ts
+++ src/calculator/my-solutions/calculator.ts
@@ -1,3 +1,7 @@
 export const calculator = (operation: string): number => {
-  return 0
+  const digits = operation.split(' ').map(n => Number(n))
+
+  const sum = digits.reduce((acc, curr) => (Number.isNaN(curr) ? acc : curr + acc), 0)
+
+  return sum
 }
