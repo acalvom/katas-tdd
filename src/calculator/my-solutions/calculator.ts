ba81d77dedc058858d5416e4d5ee51c5831f214d
acalvom <andreac.3108@gmail.com>
2024-05-10 12:54:23 +0200

    feat: calculate with two digits number - pass

--- src/calculator/my-solutions/calculator.ts
+++ src/calculator/my-solutions/calculator.ts
@@ -1,16 +1,21 @@
 export const calculator = (str: string): number => {
-  const charArray: string =
-    str.startsWith('+') || str.startsWith('-')
-      ? str.replaceAll(' ', '')
-      : '+' + str.replaceAll(' ', '')
+  let chars: string = str
+
+  if (str.startsWith('+') || str.startsWith('-')) {
+    chars = str.replace(str.charAt(0), str.charAt(0) + ' ')
+  } else {
+    chars = '+ ' + str
+  }
+  let charArray: string[] = chars.split(' ')
+
   const numberArray: number[] = []
   let operator: string = ''
 
   for (const char of charArray) {
-    if (!(char === '+' || char === '-')) {
-      operator === '+' ? numberArray.push(Number(char)) : numberArray.push(-Number(char))
-    } else {
+    if (char === '+' || char === '-') {
       operator = char
+    } else {
+      operator === '+' ? numberArray.push(Number(char)) : numberArray.push(-Number(char))
     }
   }
 
