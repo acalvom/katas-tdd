55b71c3da01ebe9300e204eddcf5f0df940fff5f
acalvom <andreac.3108@gmail.com>
2024-05-10 10:12:08 +0200

    feat: susbstract 2 numbers

--- src/calculator/my-solutions/calculator.ts
+++ src/calculator/my-solutions/calculator.ts
@@ -1,7 +1,15 @@
-export const calculator = (operation: string): number => {
-  const digits = operation.split(' ').map(n => Number(n))
+export const calculator = (str: string): number => {
+  const charArray: string = str.charAt(0).includes('+ -')
+    ? str.replaceAll(' ', '')
+    : '+' + str.replaceAll(' ', '')
+  const numberArray: number[] = []
+  let operator: string = ''
 
-  const sum = digits.reduce((acc, curr) => (Number.isNaN(curr) ? acc : curr + acc), 0)
+  for (const char of charArray) {
+    if (!(char === '+' || char === '-'))
+      operator === '+' ? numberArray.push(Number(char)) : numberArray.push(-Number(char))
+    else operator = char
+  }
 
-  return sum
+  return numberArray.reduce((acc, curr) => curr + acc, 0)
 }
